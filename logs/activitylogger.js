219ada468c7b9df0662b532d43622322afe747e9
sanchittsharma02 <sanchit.sharma@Fonixtech.io>
2025-09-27 18:20:16 +0530

    .

--- logs/activitylogger.js
+++ logs/activitylogger.js
@@ -2,24 +2,7 @@ const winston=require("winston")
 const path=require("path")
 const activitylog=require("../models/logs.model")
 
-// class MongoDBTransport extends winston.Transport {
-//     constructor(opts) {
-//       super(opts);
-//     }
-  
-//     log(info, callback) {
-//       setImmediate(() => {
-//         this.emit("logged", info);
-//       });
-//       const action = info.message;
-//       // Save log into MongoDB
-//       const { userId, details, ip, timestamp } = info;
-//       activitylog.create({ userId, action, details, ip, timestamp })
-//         .catch(err => console.error("Failed to save log to DB:", err));
-  
-//       callback();
-//     }
-//   }
+
 
 const activitylogger=winston.createLogger({
     level:"info",
